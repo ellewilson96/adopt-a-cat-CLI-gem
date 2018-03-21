8a2b707951f003e20df5535f792b3fbaaef338c9
Elle Wilson <eleanorwilsonn@gmail.com>
2018-03-21 15:04:19 +0000

    Commit with some minor changes

--- adopt_a_cat/lib/adopt_a_cat.rb
+++ adopt_a_cat/lib/adopt_a_cat.rb
@@ -1,3 +1,7 @@
+require 'open-uri'
+require 'nokogiri'
+require 'pry'
+
 require_relative './adopt_a_cat/version'
 require_relative './adopt_a_cat/cats'
 require_relative './adopt_a_cat/cli'
--- adopt_a_cat/lib/adopt_a_cat/cats.rb
+++ adopt_a_cat/lib/adopt_a_cat/cats.rb
@@ -8,24 +8,13 @@ class AdoptACat::Cats
 
   def self.scrape_cats
     cats = []
+    cats << self.scrape_anjellicle
     cats
   end
 
+  def self.scrape_anjellicle
+    doc = Nokogiri::HTML(open("https://www.anjelliclecats.com/meet-our-cats"))
+    name = doc.search("div.list-animal-name").text
+    binding.pry
+  end
 end
-
-  #  def self.scrape_cat_page("https://www.anjelliclecats.com/meet-our-cats")
-    #  cat_bio = Nokogiri::HTML(open(https://www.anjelliclecats.com/meet-our-cats))
-    #  cats = []
-      #cat_bio.css("div.list-body").each do |cat|
-      #  cat.css("td list-item").each do |info|
-        #  cat_name = info.css(".list-animal-name a").text
-        #  cat_ID = info.css(".list-animal-id").text
-        #  cat_gender = info.css(".list-animal-sexSN").text
-        #  cat_breed = info.css(".list-animal-breed").text
-        #  cats << {name: cat_name,
-        #          id: cat_ID
-        #          gender: cat_gender
-        #          breed: cat_breed}
-      #  cats
-    #  end
-    #end
