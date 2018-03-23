9a9d1ce60f978e1894c621cb01e471a13c095613
Elle Wilson <eleanorwilsonn@gmail.com>
2018-03-23 01:22:08 +0000

    Working scraper and CLI.

--- adopt_a_cat/lib/adopt_a_cat/cats.rb
+++ adopt_a_cat/lib/adopt_a_cat/cats.rb
@@ -1,5 +1,5 @@
 class AdoptACat::Cats
-  attr_accessor :name, :id, :gender, :breed, :age
+  attr_accessor :name, :gender, :breed, :age
 
   def self.today
     self.scrape_cats
@@ -12,14 +12,15 @@ class AdoptACat::Cats
   end
 
   def self.scrape_anjellicle
-    doc = Nokogiri::HTML(open("http://ws.petango.com/webservices/adoptablesearch/wsAdoptableAnimals.aspx?species=All&sex=A&agegroup=All&location=&site=&onhold=A&orderby=name&colnum=4&css=https://ws.petango.com/WebServices/adoptablesearch/css/styles.css&authkey=uqn3fk5upf1ol62utcer2x7b0sfg42ytj6j51sefh01e5u64k6&recAmount=&detailsInPopup=No&featuredPet=Include&stageID="))
-    cat = self.new
-    cat.name = doc.search(".list-animal-name").collect {|cat_name| cat_name.text}.each do |catname| catname
-    cat.id = doc.search(".list-animal-id").map {|cat_id| cat_id.text}
-    cat.gender = doc.search(".list-animal-sexSN").map {|cat_gender| cat_gender.text}
-    cat.breed = doc.search(".list-animal-breed").map {|cat_breed| cat_breed.text}
-    cat.age = doc.search(".list-animal-age").map {|cat_age| cat_age.text}
-    cat
+  doc = Nokogiri::HTML(open("http://ws.petango.com/webservices/adoptablesearch/wsAdoptableAnimals.aspx?species=All&sex=A&agegroup=All&location=&site=&onhold=A&orderby=name&colnum=4&css=https://ws.petango.com/WebServices/adoptablesearch/css/styles.css&authkey=uqn3fk5upf1ol62utcer2x7b0sfg42ytj6j51sefh01e5u64k6&recAmount=&detailsInPopup=No&featuredPet=Include&stageID="))
+  cat = self.new
+  doc.css(".list-animal-info-block").each do |catbio|
+    cat.name = catbio.css(".list-animal-name").text
+    cat.gender = catbio.css(".list-animal-sexSN").text
+    cat.breed = catbio.css(".list-animal-breed").text
+    cat.age = catbio.css(".list-animal-age").text
+  end
+  cat
 end
-end
+
 end
--- adopt_a_cat/lib/adopt_a_cat/cli.rb
+++ adopt_a_cat/lib/adopt_a_cat/cli.rb
@@ -8,22 +8,37 @@ class AdoptACat::CLI
   def list_cats
     puts "Today's Available Cats"
     @cats = AdoptACat::Cats.today
-    @cats.each.with_index(1) do |cat, i|
-    puts "#{i}. #{cat.name} - #{cat.age} - #{cat.breed}"
+    @cats.each do |cat|
+    puts "#{cat.name} - #{cat.age} - #{cat.breed} - #{cat.gender}"
    end
- end
-
+end
 
   def start
     input = nil
     if input != "exit"
-    puts "Would you like more information about any of our cats?"
+    puts "Would you like to make an appointment to meet our available cats?"
     input = gets.strip.downcase
     case input
     when "yes"
+      puts "Please call 646-457-2130 to make an appointment."
+      cancel
     when "no"
       cancel
     end
   end
+  end
+
+  def cancel
+    input = nil
+    if input != "exit"
+      puts "Return to main menu?"
+      input = gets.strip.downcase
+      case input
+      when "yes"
+        call
+      when "no"
+        puts "Please type exit to leave the cat shelter. Have a wonderful day!"
+      end
+    end
   end
 end
